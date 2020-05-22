761bc7b4852fa32f8b673c469891841e7c7301d6
AndreCatarinoUA <andrealmeida.catarino123@gmail.com>
2020-05-22 17:25:24 +0100

    Added Filter functionality by product name

---
+++ flipPhone/src/main/java/com/ua/flipPhone/item/ItemRepository.java
@@ -0,0 +1,13 @@
+package com.ua.flipPhone.item;
+
+/*
+package com.ua.flipPhone.Item;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ua.flipPhone.Item.Item;
+
+
+public interface ItemRepository extends CrudRepository<Item, String> {
+
+}*/
